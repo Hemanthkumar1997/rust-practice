cef10add1bf6800e7895411fdbb538f9047cca0d
Hemanth <hemanthmmns@gmail.com>
2023-05-07 15:10:26 +0530

    add rand crate to generate random number

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
+use rand::Rng;
 use std::io;
 
 fn main() {
-    println!("Hello, world!");
-
     println!("enter number");
+
+    let secret_num = rand::thread_rng().gen_range(1..101);
 
     let mut num = String::new();
 
     io::stdin().read_line(&mut num).expect("failed to read line");
 
-    println!("number = {}", num)
+    println!("number guessed = {}", num);
+
+    println!("real number = {}", secret_num);
 }
